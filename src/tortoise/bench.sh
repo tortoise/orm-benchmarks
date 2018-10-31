b7ad4c4388785a6cefe3dfaabf65287e1c2ceba9
Grigi <nagrigoriadis@gmail.com>
2018-10-31 08:52:27 +0200

    version bump

--- src/tortoise/bench.sh
+++ src/tortoise/bench.sh
@@ -5,12 +5,12 @@ cd $(dirname $0)
 # setup DB
 rm -f /dev/shm/db.sqlite3
 # run regular loop benchmarks
-python -m bench
+PYTHONUNBUFFERED=x python -m bench
 
 # setup DB
 rm -f /dev/shm/db.sqlite3
 # run uvloop benchmarks
-UVLOOP=1 python -m bench
+PYTHONUNBUFFERED=x UVLOOP=1 python -m bench
 
 # teardown DB
 rm -f /dev/shm/db.sqlite3
