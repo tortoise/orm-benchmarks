ff2a5875f802ac0c6ea1bee57eb87efcc5b052cb
long2ice <long2ice@gmail.com>
2022-03-21 19:05:52 +0800

    Add best orm column

--- benchmarks/present.py
+++ benchmarks/present.py
@@ -28,6 +28,7 @@ vals = [text.strip() for text in val.strip().split("\n") if text]
 data = {}
 tests = {"gm"}
 maxscore = {}
+best_orm = {}
 
 for bench in vals:
     try:
@@ -38,16 +39,23 @@ for bench in vals:
         tests.add(test)
         if float(ops) > float(maxscore.get(test, 0)):
             maxscore[test] = ops
+            best_orm[test] = orm
     except IndexError:
         pass
 
 tests = sorted(list(tests))
-groups = sorted(data.keys(), key=lambda s: s.lower()) + ["Max"]
+groups = sorted(data.keys(), key=lambda s: s.lower()) + ["Max", "Best ORM"]
 data["Max"] = maxscore
+data["Best ORM"] = best_orm
 
 for group in data.keys():
+    if group == "Best ORM":
+        continue
     gm = float(f"{geomean([float(v) for v in data[group].values()]):.2f}")
     data[group]["gm"] = gm
+
+gm_data = [data[group]["gm"] for group in data.keys() if group not in ("Best ORM", "Max")]
+best_orm["gm"] = list(data.keys())[gm_data.index(max(gm_data))]
 
 lens = [max(len(text), 10) for text in groups]
 titles = [f"{sys.argv[1]:15}"] + [f"{group:{_len}}" for group, _len in zip(groups, lens)]  # noqa
