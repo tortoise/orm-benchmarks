96ba3c135083b142dd224558ff167f2d744df99c
Dominik Gasparic <56818232+codedoga@users.noreply.github.com>
2022-10-20 23:59:51 +0200

    Update test_i.py
    updating all objects with one query is usually faste

--- benchmarks/django/simple/test_i.py
+++ benchmarks/django/simple/test_i.py
@@ -8,23 +8,19 @@ finally:
 import time
 from random import choice
 
-from django.db import transaction
 from simple.models import Journal
 
 LEVEL_CHOICE = [10, 20, 30, 40, 50]
 
-
-objs = list(Journal.objects.all())
+objs = Journal.objects.all()
 count = len(objs)
 
 start = time.time()
-
-with transaction.atomic():
-    for obj in objs:
+for obj in objs:
         obj.level = choice(LEVEL_CHOICE)
         obj.text = f"{obj.text} Update"
-        obj.save()
 
+Journal.objects.bulk_update(objs, ['level', 'text'])
 now = time.time()
 
 print(f"Django, I: Rows/sec: {count / (now - start): 10.2f}")
